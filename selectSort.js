99fd789c8ef544bb62689d35ca896ca090b2e64f
Vwalln99 <wallner.viktoria@gmx.net>
2023-12-14 14:27:26 +0100

    init commit+

--- selectSort.js
+++ selectSort.js
@@ -1,14 +1,13 @@
 "use strict";
 function selectionSort(arr) {
-    const len = arr.length;
     function swap(index1, index2) {
         const temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
-    for (let i = 0; i < len - 1; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         let minIndex = i;
-        for (let j = i + 1; j < len; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
@@ -19,7 +18,6 @@ function selectionSort(arr) {
     }
     return arr;
 }
-// Beispielaufruf
 const unsortArray = [64, 34, 25, 12, 22, 11, 90];
 const sortArray = selectionSort(unsortArray);
 console.log(sortArray);
