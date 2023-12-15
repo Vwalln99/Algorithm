dc803155a04acbac26f00c7c24cf8c4431277fc9
Vwalln99 <wallner.viktoria@gmx.net>
2023-12-15 09:01:57 +0100

    init commit

---
+++ insertSort.ts
@@ -0,0 +1,21 @@
+function insertionSort(arr: number[]): number[] {
+  
+    for (let i = 1; i < arr.length; i++) {
+      let currElement = arr[i];
+      let j = i - 1;
+  
+      while (j >= 0 && arr[j] > currElement) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+  
+      arr[j + 1] = currElement;
+    }
+  
+    return arr;
+  }
+  const unsortedArr = [64, 34, 25, 12, 22, 11, 90];
+  const sortedArr = insertionSort(unsortedArr);
+
+  console.log(sortedArr);
+  
